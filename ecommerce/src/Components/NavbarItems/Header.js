cfcc147ab3383d0d789938e87eb88196a0d6e8e6
ZuhebAkhter <zuheb1998@gmail.com>
2023-08-04 22:03:58 +0530

    protectingProductpage

--- ecommerce/src/Components/NavbarItems/Header.js
+++ ecommerce/src/Components/NavbarItems/Header.js
@@ -1,46 +1,68 @@
 import React,{useContext} from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 import CartIcon from "../../Assers/CartIcon";
 import CartContext from "../Store/CartContext";
 import './Header.css'
+import AuthContext from "../Store/AuthContext";
 
 const Header = (props) => {
    const cartCtx= useContext(CartContext);
+   const AuthCtx=useContext(AuthContext);
+   const navigate=useNavigate();
 
    const numberOfcart=cartCtx.items.reduce((curNum,item)=>{
     return curNum + item.amount;
    },0);
 // const numberOfcart=cartCtx.items.length;
+const logoutHandler=()=>{
+  AuthCtx.logout()
+  navigate('/auth')
+}
   return (
     <React.Fragment>
       
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top justify-content-center">
           <div className="container-fluid">
             <ul className="navbar-nav mx-auto p-2 ">
-              <li className="nav-item px-5">
+              <li className="nav-item">
                 <Link className="nav-link" to="/home">
                   HOME
                 </Link>
               </li>
-              <li className="nav-item">
+              {AuthCtx.isLogged && <li className="nav-item px-5">
                 <Link className="nav-link" to="/store">
                   STORE
                 </Link>
-              </li>
-              <li className="nav-item px-5">
+              </li>}
+             
+              {!AuthCtx.isLogged && <li className="nav-item px-5">
+                <Link className="nav-link" to="/auth">
+                  STORE
+                </Link>
+              </li>}
+              <li className="nav-item">
                 <Link  className="nav-link" to='/about'>
                   ABOUT
                 </Link>
               </li>
-              <li className="nav-item">
+              <li className="nav-item px-5">
                 <Link  className="nav-link" to='/contactus'>
                   CONTACT US
                 </Link>
               </li>
+              {!AuthCtx.isLogged && <li className="nav-item">
+                <Link  className="nav-link" to='/auth'>
+                  LOGIN
+                </Link>
+                </li>}
+              
+                
             </ul>
            <span className="me-5"><div>
-            <button onClick={props.onOpenCart} className="btn btn-success">YourCart({numberOfcart})</button>
-            <div className="icon me-5"><CartIcon/></div></div>
+           {AuthCtx.isLogged && <button onClick={logoutHandler} className="btn btn-danger me-2">Logout</button>}
+
+            <button onClick={props.onOpenCart} className="btn btn-success"><CartIcon/>({numberOfcart})</button>
+            </div>
            
             </span>
           </div>
